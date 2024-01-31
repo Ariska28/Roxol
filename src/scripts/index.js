adfd22075dde46a475853fbbb04cc828de12262e
Ariska28 <ariska.karmanova28@gmail.com>
2024-01-31 11:20:58 +0300

    add hero without animation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,14 @@
 import { header } from "./components/header";
+import { gsap } from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
   header();
+
+  // gsap.from("[data-hero-slider]", {
+  //   x: 2000,
+  //   duration: 3,
+  //   stagger: 3,
+  //   repeat:Infinity
+  // })
 });
 
